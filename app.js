4467e029f22501efadd731a504b434b8758d3fa7
Abhishek Kaundal <144792445+AbhishekKaundal0052@users.noreply.github.com>
2023-12-21 21:34:33 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,32 +3,20 @@ let footer = document.querySelector('footer');
 let home = document.getElementById('home')
 let random = document.getElementById("random")
 let result = document.getElementById('search-results')
-let dishNames = document.querySelectorAll('.name');
+let close1 = document.getElementById('close');
 let recipe = document.getElementById('recipe')
 
 home.addEventListener('click', function () {
     document.getElementById('descr').style.display = "block";
     title.style.display = "none";
     result.style.display = "none";
+    recipe.style.display = "none";
+    body.style.backdropFilter = "blur(0px)"
+    descr.style.filter = "blur(0px)";
+    close1.style.display = "none";
 })
 
-document.getElementById('sbutton').addEventListener('click', function () {
-    let input = document.querySelector('#result').value;
-    if (input.trim()==="") {
-        alert("Please enter a dish.");
-    }
-    else {
-        document.getElementById('descr').style.display = "none";
-        title.style.display = "block"; 
-        footer.style.marginTop = "20vh";
-        result.style.display = "grid";
-        title.innerText = `searched results for ${input}...`;
-        console.log(input);
-        showResult(input);
-    }
-});
-
-function showResult(input) {
+function showResult(input) { // to show the searched results
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${input}`)
         .then((s) => {
             console.log(s);
@@ -39,71 +27,119 @@ function showResult(input) {
             const meals = search.meals;
             var rest = '';
             meals.forEach((many) => {
-             rest += `
+                rest += `
             <div class="element">
             <img src ="${many.strMealThumb}" id="grid" alt"error">
             <br>
             <br>
-            <p class= "name"> ${many.strMeal}</p>
+            <p class= "names1"> ${many.strMeal}</p>
             </div>`
-            result.innerHTML = rest;
+                result.innerHTML = rest;
             })
         })
         .catch((error) => {
             title.innerText = "No matching results found..."
             result.innerHTML = "";
-             footer.style.marginTop = "43.6vh";
+            footer.style.marginTop = "43.6vh";
             if (error) {
-               
+
             }
             console.log(error);
         })
+
 }
-var id = 0;
-function getData() {
+
+// function getRecipe() {
+//     fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772')
+// }
+
+let names1 = document.getElementsByClassName('names1')
+document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('names1')) {
+        recipe.style.display = "flex";
+        result.style.filter = "blur(4px)";
+        title.style.filter = "blur(4px)";
+        body.style.backdropFilter = "blur(4px)";
+        recipe.style.top = "30%";
+        close1.style.display = "flex";
+        close1.style.top = "27%"
+    }
+});
+
+let names = document.querySelector('.name');
+document.getElementById('sbutton').addEventListener('click', function () {
+    let input = document.querySelector('#result').value;
+    if (input.trim() === "") {
+        alert("Please enter a dish.");
+    }
+    else {
+        document.getElementById('descr').style.display = "none";
+        title.style.display = "block";
+        footer.style.marginTop = "20vh";
+        result.style.display = "grid";
+        title.innerText = `searched results for ${input}...`;
+        console.log(input);
+        showResult(input);
+    }
+});
+
+let body = document.querySelector('body')
+let descr = document.querySelector('#descr');
+let indGrid = document.getElementById('indGrid')
+let instr = document.getElementById('instr')
+
+random.addEventListener('click', function () {
+    recipe.style.display = "flex";
+    descr.style.filter = "blur(4px)";
+    body.style.backdropFilter = "blur(4px)";
+    close1.style.display = "flex";
+    console.log('hii');
+    body.style.overflow = "hidden";
+
+});
+
+function getData() { // random meal on page
     fetch("https://www.themealdb.com/api/json/v1/1/random.php")
         .then((res) => {
-            console.log(res);
             return res.json();
         })
         .then((real) => {
-            console.log(real);
-            const meal = real.meals;
-            meal.forEach((food) => {
-                 id = food.idMeal;
+            const meal1 = real.meals;
+            meal1.forEach((meal) => {
                 random.innerHTML += `
-            <img src ="${food.strMealThumb}" id="randomImg" class="img" alt"error">
+                <img src="${meal.strMealThumb}" id="randomImg" class="img" alt="error">
+                <br><br>
+                <p class="name">${meal.strMeal}</p>
+            `;
+
+                for (let i = 1; i <= 20; i++) {
+                    if (`${meal['strIngredient' + i]}` === "" || `${meal['strIngredient' + i]}` === null) {
+                        break
+                    }
+                    else {
+                        indGrid.innerHTML += `<ul><li>${meal['strIngredient' + i]}</li></ul>`;
+                    }
+                    instr.innerHTML = `<h2>INSTRUCTIONS:</h2>
             <br>
-            <br>
-            <p class= "name"> ${food.strMeal}</p>
-            `
+            ${meal.strInstructions}`;
+                }
             })
-
         })
+        .catch((error) => {
+            console.error('Error fetching data:', error);
+        });
 }
-getData()
+getData();
 
-let indGrid = document.getElementById('indGrid')
-random.addEventListener('click', function () {
-    recipe.style.display = "block";
-    // document.getElementById('descr').style.display = "none";
-    // footer.style.marginTop = "20vh";
+close1.addEventListener('click', function () {
+    // document.getElementById('descr').style.display = "block";
+    recipe.style.display = "none";
+    body.style.backdropFilter = "blur(0px)"
+    descr.style.filter = "blur(0px)";
+    close1.style.display = "none";
+    result.style.filter = "blur(0px)";
+    title.style.filter = "blur(0px)";
+    body.style.overflow = "auto";
+})
 
-    showIndgredients()
-});
-console.log(id);
-function showIndgredients(id){
-    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772`)
-    .then((reci)=>{
-        return reci.json()
-    })
-    .then((ingredient)=>{
-        console.log(ingredient);  
-        // let ingredients = ingredient.meals;
-        // ingredients.forEach((e)=>{
-        // //     var plate = '';
-        // //     for()
-        // })
 
-    })
-}
